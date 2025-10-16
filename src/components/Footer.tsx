45bd4a7d3829952b458e7b046839c560eecb90f4
Elijah Creighton <89100976+CreightonEli@users.noreply.github.com>
2025-10-16 19:51:44 -0400

    quick bug fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function Footer() {
   const [soundEnabled, setSoundEnabled] = useState<boolean>(() => {
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 type HeaderProps = {
   difficulty: number,
   setDifficulty: (difficulty: number) => void
